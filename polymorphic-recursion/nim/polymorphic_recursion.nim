2307bd755aab54b4d36a38df5685d00ed31d3356
jaccarmac <jaccarmac@gmail.com>
2017-12-30 07:30:22 +0000

    Working Nim example.
    FossilOrigin-Name: 02c0300a5a945b28824892dc3288b3e6ac93a4a057f4cb2dbfaccb03a6a6b71c

--- polymorphic-recursion/nim/polymorphic_recursion.nim
+++ polymorphic-recursion/nim/polymorphic_recursion.nim
@@ -8,28 +8,26 @@ type
     of ilCons:
       head: int
       tail: IntList
-  NestedKind = enum
-    nEpsilon,
-    nCons,
-  Nested[T] = ref object
-    case kind: NestedKind
-    of nEpsilon: nil
-    of nCons:
-      head: T
-      tail: Nested[seq[T]]
+  Nested[T] = ref object of RootObj
+  NestedEpsilon[T] = ref object of Nested[T]
+  NestedCons[T] = ref object of Nested[T]
+    head: T
+    tail: Nested[seq[T]]
 
 proc length(il: IntList): int =
   case il.kind
   of ilNil: 0
   of ilCons: 1 + il.tail.length
 
-proc length(n: Nested): int =
-  case n.kind
-  of nEpsilon: 0
-  of nCons: 1 + n.tail.length
+method length[T](n: Nested[T]): int {.base.} = quit "override!"
+  
+method length[T](n: NestedEpsilon[T]): int = 0
+
+method length[T](n: NestedCons[T]): int = 1 + n.tail.length
+  
 
 let test_il = IntList(kind: ilCons, head: 0, tail: IntList(kind: ilCons, head: 1, tail: IntList(kind: ilNil)))
 echo test_il.length
 
-let test_nest = Nested[int](kind: nCons, head: 0, tail: Nested[seq[int]](kind: nCons, head: @[1, 2, 3], tail: Nested[seq[seq[int]]](kind: nEpsilon)))
+let test_nest = NestedCons[int](head: 0, tail: NestedCons[seq[int]](head: @[1, 2, 3], tail: NestedCons[seq[seq[int]]](head: @[@[1, 2, 3]], tail: NestedEpsilon[seq[seq[seq[int]]]]())))
 echo test_nest.length
