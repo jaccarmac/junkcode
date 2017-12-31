8ec10aab725eb3a8a818a3ccc6f2221c3d7816ff
jaccarmac <jaccarmac@gmail.com>
2017-12-31 06:59:53 +0000

    Get working example going.
    FossilOrigin-Name: 831facee98ff4e63c4a8aa09ec232a83769766385d1a5d64419630c022e8b53c

--- polymorphic-recursion/rust/src/main.rs
+++ polymorphic-recursion/rust/src/main.rs
@@ -1,27 +1,63 @@
-enum IntList {
-    Nil,
-    Cons(i64, Box<IntList>),
+trait IntList {
+    fn length(&self) -> i64;
 }
 
-impl IntList {
-    pub fn length(self) -> i64 {
-        match self {
-            IntList::Nil => 0,
-            IntList::Cons(_, t) => 1 + t.length(),
-        }
+struct IntListNil;
+struct IntListCons(i64, Box<IntList>);
+
+impl IntList for IntListNil {
+    fn length(&self) -> i64 {
+        0
+    }
+}
+
+impl IntList for IntListCons {
+    fn length(&self) -> i64 {
+        1 + self.1.length()
+    }
+}
+
+trait Nested<T> {
+    fn length(&self) -> i64;
+}
+
+struct NestedEpsilon;
+struct NestedCons<T>(T, Box<Nested<Vec<T>>>);
+
+impl<T> Nested<T> for NestedEpsilon {
+    fn length(&self) -> i64 {
+        0
+    }
+}
+
+impl<T> Nested<T> for NestedCons<T> {
+    fn length(&self) -> i64 {
+        1 + self.1.length()
     }
 }
 
 fn main() {
-    let int_list = IntList::Cons(
+    let int_list = IntListCons(
         0,
-        Box::new(IntList::Cons(
+        Box::new(IntListCons(
             1,
-            Box::new(IntList::Cons(
+            Box::new(IntListCons(
                 2,
-                Box::new(IntList::Cons(3, Box::new(IntList::Nil))),
+                Box::new(IntListCons(3, Box::new(IntListNil))),
             )),
         )),
     );
     println!("{}", int_list.length());
+
+    let nested = NestedCons::<i64>(
+        0,
+        Box::new(NestedCons::<Vec<i64>>(
+            vec![1, 2],
+            Box::new(NestedCons::<Vec<Vec<i64>>>(
+                vec![vec![3], vec![4]],
+                Box::new(NestedEpsilon),
+            )),
+        )),
+    );
+    println!("{}", nested.length());
 }
