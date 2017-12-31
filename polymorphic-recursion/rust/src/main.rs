9befc26136da7897e3014e5cfdf82d79801255be
jaccarmac <jaccarmac@gmail.com>
2017-12-31 06:42:06 +0000

    Remove nested data structure.
    FossilOrigin-Name: cc63b07abff211cd522b9b5de43e99a3f4715a5dd607c793e034ad6fee3842bc

--- polymorphic-recursion/rust/src/main.rs
+++ polymorphic-recursion/rust/src/main.rs
@@ -12,20 +12,6 @@ impl IntList {
     }
 }
 
-enum Nested<T> {
-    Epsilon,
-    Cons(T, Box<Nested<Vec<T>>>),
-}
-
-impl<T> Nested<T> {
-    pub fn length(self) -> i64 {
-        match self {
-            Nested::Epsilon => 0,
-            Nested::Cons(_, t) => 1 + t.length(),
-        }
-    }
-}
-
 fn main() {
     let int_list = IntList::Cons(
         0,
@@ -38,10 +24,4 @@ fn main() {
         )),
     );
     println!("{}", int_list.length());
-
-    let nested = Nested::Cons(
-        0,
-        Box::new(Nested::Cons(vec![1, 2, 3], Box::new(Nested::Epsilon))),
-    );
-    println!("{}", nested.length());
 }
