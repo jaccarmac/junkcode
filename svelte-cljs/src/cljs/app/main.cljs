9362f3bca2c44eead2a832ea992c0445115c5303
jaccarmac <jaccarmac@gmail.com>
2019-04-24 15:06:12 +0000

    Hack around with importing Svelte into CLJS.
    FossilOrigin-Name: b702f3bb19dbcf74b29e547e1e0870918ab0e56f3fe67f21aca19800e0f359e7

--- svelte-cljs/src/cljs/app/main.cljs
+++ svelte-cljs/src/cljs/app/main.cljs
@@ -1,1 +1,8 @@
-(ns app.main)
+(ns app.main
+  #_(:require ["/App" :default App])
+  (:require ["svelte/compiler" :as svelte]))
+
+#_(new App {:target js/document.body})
+
+(defn main! []
+  (.log js/console (.-VERSION svelte)))
