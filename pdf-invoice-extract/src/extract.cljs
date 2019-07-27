59e4906c1162a17f602b8e571c62df7fb26d3881
jaccarmac <jaccarmac@gmail.com>
2019-07-27 09:58:45 +0000

    Use async stuff.
    FossilOrigin-Name: 7f2a87b57f376caa84c775b54a6c91625c7676cbea57bf9c2a134372d00c65fa

--- pdf-invoice-extract/src/extract.cljs
+++ pdf-invoice-extract/src/extract.cljs
@@ -1,11 +1,17 @@
 (ns extract
-  (:require ["pdf.js-extract" :refer (PDFExtract)]
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :refer [chan put! <!]]
+            ["pdf.js-extract" :refer (PDFExtract)]
             ["util" :as util]))
 
 (defn main [& cli-args]
-  (.extract (new PDFExtract)
-            (first cli-args)
-            #js{}
-            #(if %1
-               (js/console.log %1)
-               (js/console.log (util/inspect %2 false nil true)))))
+  (go
+    (let [data-chan (chan)]
+      (.extract (new PDFExtract)
+                (first cli-args)
+                #js{}
+                #(if %1
+                   (js/console.log %1)
+                   (put! data-chan %2)))
+      (let [data (<! data-chan)]
+        (js/console.log (util/inspect data false nil true))))))
