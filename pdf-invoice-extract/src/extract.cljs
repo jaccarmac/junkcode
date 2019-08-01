4c9b936b5ee4472d00319a9164007a4c0d18cf10
jaccarmac <jaccarmac@gmail.com>
2019-08-01 18:32:01 +0000

    Move go block.
    FossilOrigin-Name: e0e4f7b2a3adfd85c8ccbeb9b15fa532b2c36cdce4b111d34c61de708f7fb02b

--- pdf-invoice-extract/src/extract.cljs
+++ pdf-invoice-extract/src/extract.cljs
@@ -4,18 +4,17 @@
             ["pdf.js-extract" :refer [PDFExtract]]))
 
 (defn extract [fn]
-  (go
-    (let [data-chan (chan)]
-      (.extract (new PDFExtract)
-                fn
-                #js{}
-                #(do (if %1
-                       (println %1)
-                       (put! data-chan %2))))
-      (when-let [data (<! data-chan)]
-        ;; the format we need to pull out is after a string content of "ea"
-        ;; table fmt is "ea", name, type, # ordered, # shipped, id, # U/M, total price, retail price, extended price
-        (map #(.-str %) (.-content (first (.-pages data))))))))
+  (let [data-chan (chan)]
+    (.extract (new PDFExtract)
+              fn
+              #js{}
+              #(do (if %1
+                     (println %1)
+                     (put! data-chan %2)))) 
+    (go (when-let [data (<! data-chan)]
+          ;; the format we need to pull out is after a string content of "ea"
+          ;; table fmt is "ea", name, type, # ordered, # shipped, id, # U/M, total price, retail price, extended price
+          (map #(.-str %) (.-content (first (.-pages data))))))))
 
 (defn main [& cli-args]
   (go (println (<! (extract (first cli-args))))))
