e071585b83bcfb24dde20992279fd63af339b8fe
jaccarmac <jaccarmac@gmail.com>
2022-01-06 21:09:43 +0000

    It lives!
    FossilOrigin-Name: 51dbb771d5b7de538440d6b6effe8a7afded198f7b52781cba0704b83050ce83

--- guix-gargoyle/gargoyle.scm
+++ guix-gargoyle/gargoyle.scm
@@ -1,7 +1,12 @@
 (use-modules (guix packages)
              (guix git-download)
              (guix build-system cmake)
-             (guix licenses))
+             (guix licenses)
+             (gnu packages fontutils)
+             (gnu packages image)
+             (gnu packages qt)
+             (gnu packages pkg-config)
+             (gnu packages sdl))
 
 (let ((commit "a02c2095978db492f02a0b74157d48f51edde9fa")
       (revision "1"))
@@ -18,6 +23,9 @@
        (sha256
         (base32 "008n2gq0if4mkvli7dcz9m4wwr5y4xlmipm2axrm0lsg29namr8y"))))
     (build-system cmake-build-system)
+    (arguments '(#:tests? #f))
+    (native-inputs (list pkg-config))
+    (inputs (list freetype libjpeg-turbo qtbase-5 sdl2-mixer fontconfig))
     (home-page "http://ccxvii.net/gargoyle/")
     (synopsis "An interactive fiction player")
     (description
