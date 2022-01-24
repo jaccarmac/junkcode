1004e4555ee58ad779b3155184b038de56f54693
jaccarmac <jaccarmac@gmail.com>
2022-01-24 23:59:33 +0000

    Define the package and name it explicitly at the end of the file.
    FossilOrigin-Name: 7cd42b9c4e0cc3d80b2ede83c20be1e97322704868dd2fd55d4003d0706415dd

--- guix-gargoyle/gargoyle.scm
+++ guix-gargoyle/gargoyle.scm
@@ -10,36 +10,37 @@
              (gnu packages glib)
              (gnu packages speech))
 
-(let ((commit "7942b322446b7a2ae7a99ec34c62305ac8cdeb90")
-      (revision "1"))
-  (package
-    (name "gargoyle")
-    (version (git-version "2019.1.1" revision commit))
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/garglk/garglk")
-             (commit commit)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0xk5qhsri5sj1gc95q9bw71f29a61y9ararjbd8hs0axvffybixv"))))
-    (build-system cmake-build-system)
-    (arguments '(#:configure-flags
-                 '("-DWITH_QT6=true")
-                 #:tests? #f))
-    (native-inputs (list pkg-config))
-    (inputs (list freetype
-                  libjpeg-turbo
-                  qtbase
-                  sdl2-mixer
-                  fontconfig
-                  glib
-                  speech-dispatcher))
-    (home-page "http://ccxvii.net/gargoyle/")
-    (synopsis "An interactive fiction player")
-    (description
-     "Gargoyle is an IF player that supports all the major interactive fiction formats.
+(define gargoyle
+  (let ((commit "7942b322446b7a2ae7a99ec34c62305ac8cdeb90")
+        (revision "1"))
+    (package
+      (name "gargoyle")
+      (version (git-version "2019.1.1" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/garglk/garglk")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0xk5qhsri5sj1gc95q9bw71f29a61y9ararjbd8hs0axvffybixv"))))
+      (build-system cmake-build-system)
+      (arguments '(#:configure-flags
+                   '("-DWITH_QT6=true")
+                   #:tests? #f))
+      (native-inputs (list pkg-config))
+      (inputs (list freetype
+                    libjpeg-turbo
+                    qtbase
+                    sdl2-mixer
+                    fontconfig
+                    glib
+                    speech-dispatcher))
+      (home-page "http://ccxvii.net/gargoyle/")
+      (synopsis "An interactive fiction player")
+      (description
+       "Gargoyle is an IF player that supports all the major interactive fiction formats.
 
 Most interactive fiction is distributed as portable game files. These portable
 game files come in many formats. In the past, you used to have to download a
@@ -47,4 +48,6 @@ separate player (interpreter) for each format of IF you wanted to play.
 
 Gargoyle is based on the standard interpreters for the formats it
 supports.")
-    (license license:gpl2)))
+      (license license:gpl2))))
+
+gargoyle
