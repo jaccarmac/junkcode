a0752088da2d42c4e89a1a57ed2cd195c7d37022
jaccarmac <jaccarmac@gmail.com>
2022-06-17 04:01:13 +0000

    Add Lisp script to update my Fossil repos' git export settings on GH token change.
    FossilOrigin-Name: 214f05d83a33642a0e58cbffe772e299b1d5ed767e269ff79c9f6a7001f80f8a

---
+++ fossil-git-export-passwords/fossil-git-export-passwords.lisp
@@ -0,0 +1,42 @@
+;; (replace-tokens-in-repo-directory "~/fossils/git-mirror" "ghp_newtoken")
+
+(require "asdf")
+(require "sqlite")
+(require "uiop")
+
+(use-package :sqlite)
+
+(defun current-autopush-setting (db)
+  (execute-single db "SELECT value FROM mconfig WHERE key='autopush'"))
+
+(defun mirror-state-db-path (base-path)
+  (merge-pathnames ".mirror_state/db" base-path))
+
+(defun mirror-state-db (base-path)
+  (connect (mirror-state-db-path base-path)))
+
+(defun push-url-with-new-token (url token)
+  (concatenate 'string
+               (subseq url 0 (search "ghp_" url))
+               token
+               (subseq url (position #\@ url))))
+
+(defun replace-autopush-setting (db url)
+  (execute-non-query db
+                     "REPLACE INTO mconfig(key,value) VALUES('autopush', ?)"
+                     url))
+
+(defun replace-tokens-in-repo (repo-path new-token)
+  (let ((db (mirror-state-db repo-path)))
+    (replace-autopush-setting db (push-url-with-new-token (current-autopush-setting db) new-token))
+    (disconnect db)))
+
+(defun replace-tokens-in-repo-directory (base-path new-token)
+  (dolist (repo (uiop:subdirectories base-path))
+    (replace-tokens-in-repo repo new-token)))
+
+(defun print-current-urls ()
+  (dolist (repo (uiop:subdirectories "."))
+    (let ((db (mirror-state-db repo)))
+      (format t "~a~%" (current-autopush-setting db))
+      (disconnect db))))
